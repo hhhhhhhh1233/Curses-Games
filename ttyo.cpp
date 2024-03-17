0e99eba9cca212f18f291f09d1ba8dbdb534a72a
tage <tagetjerngren@gmail.com>
2024-03-17 17:59:43 +0100

    Seemingly fixed the puyos falling out of bounds

--- ttyo.cpp
+++ ttyo.cpp
@@ -1,4 +1,5 @@
 #include <curses.h>
+#include <iostream>
 #include <vector>
 #include <unistd.h>
 
@@ -8,6 +9,7 @@ constexpr int UP = 65;
 constexpr int DOWN = 66;
 constexpr int RIGHT = 67;
 constexpr int LEFT = 68;
+constexpr int SPACE = 32;
 
 constexpr char EMPTYGRID = '*';
 constexpr char SETPUYO = 'D';
@@ -15,30 +17,48 @@ constexpr char ACTIVEPUYO = 'P';
 
 constexpr int FRAMESTOSLEEP = 10000;
 
+constexpr int EMPTYFIELD_PAIR = 1;
+constexpr int REDPUYOFIELD_PAIR = 2;
+constexpr int BLUEPUYOFIELD_PAIR = 3;
+
+enum Color { red, blue, yellow, green, purple };
+
 struct Vec2
 {
 	int x;
 	int y;
 
-	Vec2 (int X, int Y)
+	Vec2 (int X = 0, int Y = 0)
 	{
 		x = X;
 		y = Y;
 	}
 };
 
-struct Node
+class PuyoNode
 {
-	int x;
-	int y;
+public:
+	Vec2 Position;
+	Color Type;
+
+	PuyoNode(Vec2 pos, Color type)
+	{
+		Position = pos;
+		Type = type;
+	}
+};
+
+struct Puyo
+{
+	PuyoNode Pivot;
+	PuyoNode Tagalong;
+
 	char Body;
 	Vec2 rotations[4] = { Vec2(0, -1), Vec2(1, 0), Vec2(0, 1), Vec2(-1, 0) };
 	int currentRotation;
 
-	Node(int X, int Y, int body)
+	Puyo(int X, int Y, Color t, int body) : Pivot(PuyoNode(Vec2(), t)), Tagalong(PuyoNode(Vec2(), t))
 	{
-		x = X;
-		y = Y;
 		Body = body;
 		currentRotation = 0;
 	}
@@ -47,6 +67,7 @@ struct Node
 	{
 		currentRotation++;
 		currentRotation = currentRotation % 4;
+		UpdateTagalong();
 	}
 
 	void CCW()
@@ -54,22 +75,42 @@ struct Node
 		currentRotation--;
 		if (currentRotation == -1)
 			currentRotation = 3;
+		UpdateTagalong();
 	}
 
-	void Draw()
+	void MoveLeft()
 	{
-		move(y, x);
-		printw("%c", Body);
-		move(y + rotations[currentRotation].y, x + rotations[currentRotation].x);
-		printw("%c", 'O');
+		Pivot.Position.x--;
+		UpdateTagalong();
+	}
+
+	void MoveRight()
+	{
+		Pivot.Position.x++;
+		UpdateTagalong();
+	}
+
+	void MoveDown()
+	{
+		Pivot.Position.y++;
+		UpdateTagalong();
+	}
+
+	void UpdateTagalong()
+	{
+		Tagalong.Position = Vec2(Pivot.Position.x + rotations[currentRotation].x,Pivot.Position.y + rotations[currentRotation].y);
 	}
 
 	void Draw(int X, int Y)
 	{
-		move(y + Y, x + X);
+		//attron(COLOR_PAIR(REDPUYOFIELD_PAIR));
+		move(Pivot.Position.y + Y, Pivot.Position.x + X);
 		printw("%c", Body);
-		move(y + Y + rotations[currentRotation].y, x + X + rotations[currentRotation].x);
+		//attroff(COLOR_PAIR(REDPUYOFIELD_PAIR));
+		//attron(COLOR_PAIR(BLUEPUYOFIELD_PAIR));
+		move(Tagalong.Position.y + Y, Tagalong.Position.x + X);
 		printw("%c", 'O');
+		//attroff(COLOR_PAIR(BLUEPUYOFIELD_PAIR));
 	}
 };
 
@@ -90,14 +131,14 @@ public:
 
 	char GetChar(int x, int y)
 	{
-		if (x < 0 || y < 0 || y > width || x > height)
+		if (x < 0 || y < 0 || y > width - 1 || x > height)
 			return ' ';
 		return s[x * width + y];
 	}
 
 	void SetChar(int x, int y, char c)
 	{
-		if (x < 0 || y < 0 || y > width || x > height)
+		if (x < 0 || y < 0 || y >= width || x >= height)
 			return;
 		s[x * width + y] = c;
 	}
@@ -107,16 +148,16 @@ public:
 		return GetChar(y, x) == EMPTYGRID;
 	}
 
-	void SetActive(Node n)
+	void SetActive(Puyo n)
 	{
-		SetChar(n.x, n.y, ACTIVEPUYO);
-		SetChar(n.x + n.rotations[n.currentRotation].x, n.y + n.rotations[n.currentRotation].y, ACTIVEPUYO);
+		SetChar(n.Pivot.Position.y, n.Pivot.Position.x, ACTIVEPUYO);
+		SetChar(n.Pivot.Position.y + n.rotations[n.currentRotation].y, n.Pivot.Position.x + n.rotations[n.currentRotation].x, ACTIVEPUYO);
 	}
 
-	void SetOccupied(Node n)
+	void SetOccupied(Puyo n)
 	{
-		SetChar(n.y, n.x, SETPUYO);
-		SetChar(n.y + n.rotations[n.currentRotation].y, n.x + n.rotations[n.currentRotation].x, SETPUYO);
+		SetChar(n.Pivot.Position.y, n.Pivot.Position.x, SETPUYO);
+		SetChar(n.Pivot.Position.y + n.rotations[n.currentRotation].y, n.Pivot.Position.x + n.rotations[n.currentRotation].x, SETPUYO);
 	}
 
 	void Draw(int X, int Y)
@@ -125,8 +166,10 @@ public:
 		{
 			for (int y = 0; y < height; y++)
 			{
+				//attron(COLOR_PAIR(EMPTYFIELD_PAIR));
 				move(y + Y, x + X);
 				printw("%c", GetChar(y, x));
+				//attroff(COLOR_PAIR(EMPTYFIELD_PAIR));
 			}
 		}
 	}
@@ -137,47 +180,58 @@ class Field
 public:
 	Vec2 GridPosition;
 	Grid grid;
-	Node ActivePuyo;
-	vector<Node> SetPuyos;
+	Puyo ActivePuyo;
+	vector<Puyo> SetPuyos;
 
-	Field(Vec2 gridPos, int width, int height) : GridPosition(gridPos), grid(Grid(width, height)), ActivePuyo(Node(width/2, 0, 'X')) {}
+	Field(Vec2 gridPos, int width, int height) : GridPosition(gridPos), grid(Grid(width, height)), ActivePuyo(Puyo(width/2, 0, Color::red, 'X')) {}
 
 	void PuyoFall()
 	{
-		int attachedX = ActivePuyo.x + ActivePuyo.rotations[ActivePuyo.currentRotation].x;
-		int attachedY = ActivePuyo.y + ActivePuyo.rotations[ActivePuyo.currentRotation].y;
-		if (grid.IsEmpty(ActivePuyo.x, ActivePuyo.y + 1) && grid.IsEmpty(attachedX, attachedY + 1))
-			ActivePuyo.y++;
+		if (grid.IsEmpty(ActivePuyo.Pivot.Position.x, ActivePuyo.Pivot.Position.y + 1) && grid.IsEmpty(ActivePuyo.Tagalong.Position.x, ActivePuyo.Tagalong.Position.y + 1))
+			ActivePuyo.MoveDown();
 		else
 		{
 			SetPuyos.push_back(ActivePuyo);
 			grid.SetOccupied(ActivePuyo);
-			ActivePuyo = Node(grid.width/2, 0, 'X');
+//			move(50,50);
+			//printw("%d", grid.width/2);
+			//refresh();
+			//sleep(2);
+			ActivePuyo = Puyo(grid.width/2, 0, Color::red, 'X');
 		}
 	}
 
 	void PuyoMoveLeft()
 	{
-		int attachedX = ActivePuyo.x + ActivePuyo.rotations[ActivePuyo.currentRotation].x;
-		int attachedY = ActivePuyo.y + ActivePuyo.rotations[ActivePuyo.currentRotation].y;
-		if (grid.IsEmpty(ActivePuyo.x - 1, ActivePuyo.y) && grid.IsEmpty(attachedX - 1, attachedY))
-			ActivePuyo.x--;
+		if (grid.IsEmpty(ActivePuyo.Pivot.Position.x - 1, ActivePuyo.Pivot.Position.y) && grid.IsEmpty(ActivePuyo.Tagalong.Position.x - 1, ActivePuyo.Tagalong.Position.y))
+			ActivePuyo.MoveLeft();
 	}
 	
 	void PuyoMoveRight()
 	{
-		int attachedX = ActivePuyo.x + ActivePuyo.rotations[ActivePuyo.currentRotation].x;
-		int attachedY = ActivePuyo.y + ActivePuyo.rotations[ActivePuyo.currentRotation].y;
-		if (grid.IsEmpty(ActivePuyo.x + 1, ActivePuyo.y) && grid.IsEmpty(attachedX + 1, attachedY))
-			ActivePuyo.x++;
+//		move(50,50);
+//		printw("%c", grid.GetChar(ActivePuyo.Pivot.Position.x + 1, ActivePuyo.Pivot.Position.y));
+//		refresh();
+//		sleep(1);
+		if (grid.IsEmpty(ActivePuyo.Pivot.Position.x + 1, ActivePuyo.Pivot.Position.y) && grid.IsEmpty(ActivePuyo.Tagalong.Position.x + 1, ActivePuyo.Tagalong.Position.y))
+			ActivePuyo.MoveRight();
+	}
+
+	void PuyoDrop()
+	{
+		while (grid.IsEmpty(ActivePuyo.Pivot.Position.x, ActivePuyo.Pivot.Position.y + 1) && grid.IsEmpty(ActivePuyo.Tagalong.Position.x, ActivePuyo.Tagalong.Position.y + 1))
+		{
+			PuyoFall();
+		}
+		PuyoFall();
 	}
 	
 	void Draw()
 	{
 		grid.Draw(GridPosition.x, GridPosition.y);
+		//for (auto puyo : SetPuyos)
+		//	puyo.Draw(GridPosition.x, GridPosition.y);
 		ActivePuyo.Draw(GridPosition.x, GridPosition.y);
-		for (auto puyo : SetPuyos)
-			puyo.Draw(GridPosition.x, GridPosition.y);
 	}
 
 };
@@ -195,18 +249,29 @@ int main()
 	// Initiate the screen
 	initscr();
 
+	if (!has_colors())
+	{
+		endwin();
+		std::cout << "Your terminal does not support color\n";
+		exit(1);
+	}
+
 	// Configuration
 	noecho();				// Don't echo input to the screen
 	curs_set(0);			// Hide the cursor
 	nodelay(stdscr, TRUE);	// Make input nonblocking
+	//start_color();			// Allows us to have colors in our game 
 
+	init_pair(EMPTYFIELD_PAIR, COLOR_WHITE, COLOR_WHITE);
+	init_pair(REDPUYOFIELD_PAIR, COLOR_RED, COLOR_RED);
+	init_pair(BLUEPUYOFIELD_PAIR, COLOR_BLUE, COLOR_BLUE);
 	while (!quit)
 	{
-		// If there is input, put it on screen
+		move(50,50);
+		printw("%d, %d", f.ActivePuyo.Pivot.Position.x, f.ActivePuyo.Pivot.Position.y); 
 		if ((ch = getch()) != ERR)
 		{
 			clear();
-			move(1, 1);
 			if (ch == UP)
 				f.ActivePuyo.CW();
 			else if (ch == DOWN)
@@ -215,6 +280,8 @@ int main()
 				f.PuyoMoveRight();
 			else if (ch == LEFT)
 				f.PuyoMoveLeft();
+			else if (ch == SPACE)
+				f.PuyoDrop();
 		}
 
 		// Only drop the puyo every 10000 frames
@@ -222,7 +289,6 @@ int main()
 		{
 			clear();
 			f.PuyoFall();
-			move(3,1);
 		}
 		f.Draw();
 		refresh();
